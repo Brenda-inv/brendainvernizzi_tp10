e1dd664b7aa1edac5c66e52d4ec2a0821cdae853
Brenda-inv <brendamagaliinvernizzi@gmail.com>
2024-11-29 20:53:33 -0300

    tp 10 terminado ahora si

--- boletos.js
+++ boletos.js
@@ -4,10 +4,10 @@ let edad= 34;
 let número_de_teléfono=11_02202202;
 let socio= true;
 let fecha_del_partido="15 de mayo del 2009";
-let hora_del_partido="16y20";
+let hora_del_partido="16 y 20";
 let número_de_silla=153;
 let precio_del_boleto= 45000.64
 
 
-
 console.log()
+console.log(typeof hora_del_partido)
